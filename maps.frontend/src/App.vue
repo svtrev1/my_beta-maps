d2c5fa446e256a405b33cc979602a3cab8efa3b5
svtrev <revtsova_st@edu.surgu.ru>
2025-02-28 17:50:17 +0500

    change backend to php

--- maps.frontend/src/App.vue
+++ maps.frontend/src/App.vue
@@ -1,46 +1,35 @@
-<script setup>
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div id="app">
+    <Map />
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+import Map from './components/Map.vue';
+
+export default {
+  components: {
+    Map,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  /* Убираем фиксированные размеры */
+  width: 100%;
+  height: 100%;
+  padding: 0;
+  margin: 0;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+html, body {
+    height: 100%;
+    width: 100%;
+    margin: 0;
+    padding: 0;
 }
 </style>
---
+++ maps.frontend/src/components/Map.vue
@@ -0,0 +1,40 @@
+<template>
+    <div id="map" class="map"></div>
+  </template>
+  
+  <script>
+  import 'ol/ol.css';
+  import { Map, View } from 'ol';
+  import { Tile as TileLayer } from 'ol/layer';
+  import { OSM } from 'ol/source';
+  
+  export default {
+    name: 'Map',
+    mounted() {
+      this.initializeMap();
+    },
+    methods: {
+      initializeMap() {
+        new Map({
+          target: 'map',
+          layers: [
+            new TileLayer({
+              source: new OSM(),
+            }),
+          ],
+          view: new View({
+            center: [0, 0], // Координаты центра карты (например, 0, 0 для начала)
+            zoom: 4, // Уровень масштабирования
+          }),
+        });
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .map {
+    width: 100%; height: 100%; position: relative;
+  }
+  </style>
+  
